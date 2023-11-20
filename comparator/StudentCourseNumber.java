2dde3fec523b7223d57949d1d1a87d3d6f2a008c
Igor-Kir <127873172+Igor-Kir@users.noreply.github.com>
2023-11-20 22:58:30 +0300

    Delete comparator directory

--- comparator/StudentCourseNumber.java
+++
@@ -1,9 +0,0 @@
-package comparator;
-import model.Student;
-
-public class StudentCourseNumber implements StudentComparator{
-    @Override
-    public int compare(Student o1, Student o2) {
-        return  Integer.compare(o1.getCurrentCourseNumber(), o2.getCurrentCourseNumber());
-    }
-}
